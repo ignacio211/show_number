672b7b077ce0258be3c990f57a11f97b79392b31
Ignacio <ignacio.gormaz199@gmail.com>
2019-06-14 03:00:56 -0400

    muestra numero

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: ignacio
+ *
+ * Created on 14 de junio de 2019, 2:52
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int a;
+    cout << "Ingrese primer numero: ";
+    cin >> a;
+    cout << a;
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
